19a0720346576d97b5ed36c7a2103a3bba98a5b8
Jason Zeng <jason.zeng@intel.com>
2022-06-15 09:43:51 +0800

    adapt to new version of nvim-tree

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,35 +1,3 @@
-local g = vim.g
-
-g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
-g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
-g.nvim_tree_root_folder_modifier = ":~" -- This is the default. See :help filename-modifiers for more options
-g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
-g.nvim_tree_group_empty = 1 --  0 by default, compact folders that only contain a single folder into one node in the file tree
-g.nvim_tree_icon_padding = " " -- one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
-g.nvim_tree_symlink_arrow = " >> " --  defaults to ' ➛ '. used as a separator between symlinks' source and target.
-g.nvim_tree_respect_buf_cwd = 1 -- 0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
-
-g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1, folder_arrows = 1 }
-g.nvim_tree_icons = {
-	default = "",
-	symlink = "",
-	git = {
-		unstaged = "",
-		staged = "S",
-		unmerged = "",
-		renamed = "➜",
-		deleted = "",
-		untracked = "U",
-		ignored = "◌",
-	},
-	folder = {
-		default = "",
-		open = "",
-		empty = "",
-		empty_open = "",
-		symlink = "",
-	},
-}
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
 require("nvim-tree").setup({
 	-- disables netrw completely
@@ -46,6 +14,7 @@ require("nvim-tree").setup({
 	hijack_cursor = true,
 	-- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
 	update_cwd = true,
+	respect_buf_cwd = true, -- false by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
 	-- show lsp diagnostics in the signcolumn
 	diagnostics = {
 		enable = true,
@@ -96,6 +65,37 @@ require("nvim-tree").setup({
 		},
 	},
 	renderer = {
+		highlight_git = true,
+		highlight_opened_files = "icon",
+		root_folder_modifier = ":~",
+		add_trailing = true,
+		group_empty = true,
+		icons = {
+			padding = " ",
+			symlink_arrow = " >> ",
+			show = { git = true, folder = true, file = true, folder_arrow = true },
+			glyphs = {
+				default = "",
+				symlink = "",
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					deleted = "",
+					untracked = "U",
+					ignored = "◌",
+				},
+				folder = {
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+				},
+			},
+		},
+
 		indent_markers = {
 			enable = true,
 			icons = {
