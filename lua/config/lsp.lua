8f5d1c18362b08bc194ad7573528dcfcc91b6654
Jason Zeng <jason.zeng@intel.com>
2022-10-11 03:11:38 +0800

    lsp: client.resolved_capability is deprecated

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -3,8 +3,8 @@ local nvim_lsp = require("lspconfig")
 local opts = { noremap=true, silent=true }
 
 local on_attach = function(client, bufnr)
-	client.resolved_capabilities.document_formatting = false
-	client.resolved_capabilities.document_range_formatting = false
+	client.server_capabilities.document_formatting = false
+	client.server_capabilities.document_range_formatting = false
 
 	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
